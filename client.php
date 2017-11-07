a8b0d07be26ec7e661f558801cb549d2a77c6af6
ahmadsuhaili <ahmadsuhaili244@gmail.com>
2017-11-07 15:29:17 +0700

    update page

---
+++ client.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Demo Web</title>
+		<link rel="stylesheet" type="text/css" href="themes/default/assets/css/semantic.css">
+		<link rel="stylesheet" type="text/css" href="themes/default/assets/css/flexslider.css" media="screen" />
+		<link rel="stylesheet" type="text/css" href="themes/default/assets/css/custom.css">
+	</head>
+	<body>
+
+		<div class=" container-wrapper">
+		 <?php include ("menu-bhs.php")  ?>
+
+			<div class="ui container segment">
+			<?php include ("submenu-bhs-partner.php")  ?>
+			</div>			
+
+			<div class="content-others">
+				<div class="top-content"></div>
+				<div class="content-inner">
+					<div class="content-images">
+						<img class="" src="themes/default/assets/images/wireframe-img.png" style="object-fit:cover">
+					</div>
+					<div class="content-descriptions">
+						<h3>Klien Kami</h3>
+
+						<div class="ui grid" style="margin-bottom: 20px;">
+						  	<div class="eight wide column">
+						  		<div class="ui list">
+							  <div class="item"><p>PT. Permata Hijau Group </p></div>
+							  <div class="item"><p>PT. Industri Sandang Nusantara (Persero) </p></div>
+							  <div class="item"><p>PT. Mitra Bersama Abadi </p></div>
+							  <div class="item"><p>PT. Gunung Gahapi </p></div>
+							  <div class="item"><p>PT. Berlian Transtar Abadi</p></div>
+							  <div class="item"><p>PT. Karya Sumber Daya</p></div>
+								<div class="item"><p>PT. Genesis Komunika International  </p></div>
+								<div class="item"><p>PT. Grand Mulia Mandiri</p></div>
+								<div class="item"><p>PT. Lematang Group	</p></div>
+								<div class="item"><p>PT. Ulima Nitra Group</p></div>
+								<div class="item"><p>PT. Mikitex Pratama</p></div>
+								<div class="item"><p>PT. Indopasific Sheraton</p></div>
+								<div class="item"><p>PT. Pancaran Samudera Transport</p></div>
+								<div class="item"><p>PT. Titan Mining Energy</p></div>
+								<div class="item"><p>PT. Ridho Jaya</p></div>
+								<div class="item"><p>PT. Senamas Energindo Mineral</p></div>
+								<div class="item"><p>PT. Christy Sejahtera</p></div>
+								<div class="item"><p>PT. Central Mega Kencana Group</p></div>
+								<div class="item"><p>PT. Adyawinsa</p></div>
+								<div class="item"><p>PT. GHS Infracom Telesarana</p></div>
+								<div class="item"><p>PT. Jawara Kreasi Cemerlang Apartemen Permata Eksklusif</p></div>
+								<div class="item"><p>PT. Mabar Feed</p></div>
+								
+								
+								
+								</div>
+						  	</div>
+
+						  	<div class="eight wide column">
+						  		<div class="ui list">
+								  <div class="item"><p>PT. Indah Logistik &amp; Grup</p></div>
+								  <div class="item"><p>PT. Wijaya Karya</p></div>
+								  <div class="item"><p>PT. Transmigasindo</p></div>
+								  <div class="item"><p>PT. Dana Unico Finance</p></div>
+								  <div class="item"><p>PT. Basuki Rahmanta Putra</p></div>
+								  <div class="item"><p>PT. Sinar Mandiri Sukses</p></div>
+								  <div class="item"><p>PT. Lumintuna Marine Service</p></div>
+								  <div class="item"><p>PT. Purnama Sari Mandiri</p></div>
+								  <div class="item"><p>PT. Parastar Echorindo</p></div>
+								  <div class="item"><p>PT. Proton Liftindo Perkasa</p></div>
+								  <div class="item"><p>PT. Servier Indonesia</p></div>
+								  <div class="item"><p>PT. Yanuar Perkasa Samudra</p></div>
+								  <div class="item"><p>PT. Tri Sukses Wanatama</p></div>
+								  <div class="item"><p>PT. Sumber Kreasi Cipta Logam</p></div>
+								  <div class="item"><p>PT. Tunas Binatama Lestari</p></div>
+								  <div class="item"><p>PT. Graham Packaging Indonesia</p></div>
+								  <div class="item"><p>PT. Widya Sapta Colas</p></div>
+								  <div class="item"><p>PT. Sistim Solusindo Internasional</p></div>
+								  <div class="item"><p>PT. Perfetti Van Melle Indonesia PT. East West Seed </p></div>
+								  <div class="item"><p>PT. Riamu Group PT. Berkat Maritim Abadi</p></div>
+								  <div class="item"><p>PT. Alkon Rikonindo PT. Karsa Tunggal Eka Mandiri</p></div>
+								  <div class="item"><p>PT. Jaya Mukti Berlian</p></div>
+								</div>
+						  	</div>
+						</div>
+
+						<p style="text-align: center;">Dan klien korporasi serta individual lainnya...</p>
+
+						<blockquote><p>Komitmen kami adalah senantiasa menjaga hubungan baik
+						dengan para klien dalam memenuhi dan melayani kebutuhan asuransi serta dalam penyelesaian masalah klaim.</p></blockquote>
+
+						
+					</div>
+				</div>
+				<div class="clearfix"></div>
+				<div class="top-content" style="margin-top:-64px;"></div>
+			</div>
+
+	        <?php include ("footer.php")  ?>
+		</div>
+
+		<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+        <script src="themes/default/assets/js/semantic.js"></script>
+        <script src="themes/default/assets/js/search.js"></script>
+        
+	</body>
+</html>
---
+++ submenu-bhs-partner.php
@@ -0,0 +1,6 @@
+<div class="ui breadcrumb">
+<a class="section <?php if(basename($_SERVER['SCRIPT_NAME']) == 'client.php'){echo 'active'; }else { echo ''; } ?>" href="client.php">Klien</a>
+<i class="right angle icon divider"></i>
+<a class="<?php if(basename($_SERVER['SCRIPT_NAME']) == 'insurance.php'){echo 'active'; }else { echo ''; } ?> section" href="insurance.php">Rekanan Asuransi</a>
+
+</div>
