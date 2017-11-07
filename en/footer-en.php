0845db4f38dc50ad35e44a39658a8bedc04c51e0
aleeas <ahmadsuhaili244@gmail.com>
2017-11-07 19:28:36 +0700

    commit

--- en/footer-en.php
+++ en/footer-en.php
@@ -1,14 +1,15 @@
 <footer>
 	<div class="footer-content">
-		<img src="../themes/default/assets/images/Logo ARMS-01.png" height="60">
+		<!-- <img src="../themes/default/assets/images/Logo ARMS-01.png" height="60"> -->
 		<div class="center-content">"We make your insurance needs to be Efficient, Effective and Reliable"</div>
 		<ul class="list-corporate">
+			<li><span>Terdaftar dan Diawasi oleh :</span></li>
 			<li><a href="#!"><img src="../themes/default/assets/images/OJK_Logo.png" height="40"></a></li>
 			<li><a href="#!"><img src="../themes/default/assets/images/mari-berasuransi.png" height="40"></a></li>
 			<li><a href="#!"><img src="../themes/default/assets/images/aparindo logo.jpg" height="40"></a></li>
 		</ul>
 	</div>
 	<div class="copyright-text">
-		&copy; Copyright 2017 ARMS Insurance, All Right Reserved
+		&copy; Copyright 2017 PT. Alih Risiko Makna Sejahtera. All Rights Reserved.
 	</div>
 </footer>
