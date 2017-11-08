a23561cbe40143e8589e46f13ba5b583985c470d
aleeas <ahmadsuhaili244@gmail.com>
2017-11-08 22:33:34 +0700

    footer en

--- en/footer-en.php
+++ en/footer-en.php
@@ -1,13 +1,13 @@
 <footer>
 	<div class="footer-content">
-		<!-- <img src="../themes/default/assets/images/Logo ARMS-01.png" height="60"> -->
+		<img src="../themes/default/assets/images/Logo ARMS-01.png" height="40">
 		<div class="center-content">"We make your insurance needs to be Efficient, Effective and Reliable"</div>
 		<ul class="list-corporate">
 			<li><span>Terdaftar dan Diawasi oleh :</span></li>
-			<li><a href="#!"><img src="../themes/default/assets/images/OJK_Logo.png" height="40"></a></li>
-			<li><a href="#!"><img src="../themes/default/assets/images/mari-berasuransi.png" height="40"></a></li>
-			<li><a href="#!"><img src="../themes/default/assets/images/aparindo logo.jpg" height="40"></a></li>
-			<li><a href="#!"><img src="../themes/default/assets/images/robema.jpeg" height="30"></a></li>
+			<li><a href="#!"><img src="../themes/default/assets/images/OJK_Logo.png" height="30"></a></li>
+			<li><a href="#!"><img src="../themes/default/assets/images/mari-berasuransi.png" height="30"></a></li>
+			<li><a href="#!"><img src="../themes/default/assets/images/aparindo logo.jpg" height="30"></a></li>
+			<li><a href="#!"><img src="../themes/default/assets/images/robema.jpeg" height="25"></a></li>
 		</ul>
 	</div>
 	<div class="copyright-text">
