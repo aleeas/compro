49376922a35b0aa6b7542f977f8dfb27d66a9e42
aleeas <ahmadsuhaili244@gmail.com>
2017-11-12 21:52:37 +0700

    fix js

--- themes/default/assets/js/search.js
+++ themes/default/assets/js/search.js
@@ -24,34 +24,42 @@ var content = [
 	description: 'Insurance Program',
 	url: 'insurance-program.php'
 	},
+	{
 	title: 'Career',
 	description: 'Career',
 	url: 'career.php'
 	},
+	{
 	title: 'Contact',
 	description: 'Contact',
 	url: 'contact.php'
 	},
+	{
 	title: 'Client',
 	description: 'Client Program',
 	url: 'client.php'
 	},
+	{
 	title: 'Management',
 	description: 'Management ',
 	url: 'management.php'
 	},
+	{
 	title: 'Work Philosopy',
 	description: 'Work Philosopy Program',
 	url: 'work-philosopy.php'
 	},
+	{
 	title: 'Insurance',
 	description: 'Insurance',
 	url: 'insurance.php'
 	},
+	{
 	title: 'Our Team',
 	description: 'Our Team',
 	url: 'our-team.php'
 	},
+	{
 	title: 'Legality',
 	description: 'Legality',
 	url: 'legality.php'
