e3811ef12a4959650f327694e874b690d6f7c146
aleeas <ahmadsuhaili244@gmail.com>
2017-11-12 09:17:17 +0700

    fixing

--- contact.php
+++ contact.php
@@ -12,20 +12,82 @@
 		  	<?php include ("menu-bhs.php")  ?>			
 
 			<div class="content-others">
-				<div class="top-content"></div>
+
+				<div class="ui message" style="position: absolute; top: 60%;  left: 63%; z-index: 10; display:none">
+				  <div class="header">
+				   Terima Kasih
+				  </div>
+				  <p>Terimakasih telah menghubungi kami!</p>
+				</div>
+				
 				<div class="content-inner">
 					<div class="content-images">
+						<div class="top-content-left"></div>
 						<img class="" src="themes/default/assets/images/wireframe-img.png" style="object-fit:cover">
 					</div>
 					<div class="content-descriptions">
+						<div class="top-content-right"></div>
 						<h3>Kontak Kami</h3>
-						<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. <br><br>
-						The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+						<div style="margin-top:10px;">
+						<p>Untuk informasi lebih lanjut silahkan menghubungi kami.</p>
+
+						<div class="ui form">
+						  <div class="inline fields">
+						    <div class="sixteen wide field">
+						      <label>Nama</label>
+						      <input type="text" placeholder="Nama" style="margin-left:-2px;">
+						    </div>
+						    </div>
+						    <div class="inline fields">
+							    <div class="sixteen wide field">
+							      <label>Email</label>
+							      <input type="text" placeholder="Email">
+							    </div>
+						    </div>
+						    <div class="inline fields">	
+							    <div class="sixteen wide field">
+							      <label>Subjek</label>
+							      <input type="text" placeholder="Subjek" style="margin-left:-5px;">
+							    </div>
+						    </div>
+						    <div class="inline fields">
+							    <div class="sixteen wide field">
+							      <label>Pesan</label>
+							      <textarea></textarea>
+							    </div>
+						    </div>
+						    <div style="text-align:right;margin-bottom:20px;">
+						    	<button class="ui button trigger" type="submit" onClick="window.location.reload()">Submit</button>
+						    </div>
+
+						    <div class="inline fields">
+							    <div class="sixteen wide field">
+							    	<label>Alamat</label>
+							    	<address>
+							    	PT ALIH RISIKO MAKNA SEJATERA (ARMS) <br>
+									Gedung Yarnati t. 3 R. 309 <br>
+									Jl. Proklamasi No.44, Jakarta Pusat - 10320 <br>
+									Telp. (021) 2139 7548 / 75
+							    	</address>
+							    	<span style="position:relative;left:35px;"><img src="themes/default/assets/images/phone.png"></span>
+								    
+								</div>
+							</div>
+						    
+						    
+						    							    
+						  </div>
+						</div>
+						</div>
 					</div>
+					<div class="ui grid" style="width:955px">
+						<div class="seven wide column" style="padding-top:0;"><div class="top-content-right-footer" style="width:100%"></div></div>
+						<div class="nine wide column" style="padding-top:0;"><div class="top-content-left-footer" style="width:100%"></div>	</div>
+					</div>
+					<div class="clearfix"></div>
+
+					<div class="divider-content"></div>
 				</div>
-				<div class="clearfix"></div>
-				<div class="top-content" style="margin-top:-64px;"></div>
-			</div>
 
 	        <?php include ("footer.php")  ?>
 		</div>
@@ -34,6 +96,17 @@
 	    crossorigin="anonymous"></script>
         <script src="themes/default/assets/js/semantic.js"></script>
         <script src="themes/default/assets/js/search.js"></script>
+
+        <script type="text/javascript">
+     //    	$('.message').on('click', function() {
+			  //   $(this).show();
+			  // });
+
+        	$(".trigger").click(function(){
+			    $(".message").show();
+			    reload();
+			});
+        </script>
         
 	</body>
 </html>
