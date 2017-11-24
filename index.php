5453c74ed8d12003fd01cd79e24d8fb01bc59268
aleeas <ahmadsuhaili244@gmail.com>
2017-11-24 21:39:26 +0700

    fix crash

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
 	          <ul class="slides">
 	            	<li>
 	            		<div class="carousel-card">
-		            		<img src="themes/default/assets/images/Asuransi Harta Benda - Property All Risk-01.png" style="width:104px;height:104px;object-fit:cover" alt="http://mpm-insurance.com/0_repository/images/61_1small-liability.png">
+		            		<img src="themes/default/assets/images/Asuransi Harta Benda - Property All Risk-01.png" style="width:104px;height:104px;object-fit:cover" alt="">
 		            		<div class="carousel-card-content">
 		            			<h5>Asuransi Harta Benda</h5>
 		            			<p class="line-clamp line-clamp-3">Asuransi Kebakaran, Asuransi Industrial/Property All Risks, Asuransi Gempa Bumi, Asuransi Sabotase & Terorisme & Lainnya.</p>
@@ -69,7 +69,7 @@
 
 	            	<li>
 	            		<div class="carousel-card">
-		            		<img src="themes/default/assets/images/Asuransi Kendaraan Bermotor - Motor Vehicle Insurance-02.png" style="width:104px;height:104px;object-fit:cover" alt="http://mpm-insurance.com/0_repository/images/62_1heavy-equipment2.jpg">
+		            		<img src="themes/default/assets/images/Asuransi Kendaraan Bermotor - Motor Vehicle Insurance-02.png" style="width:104px;height:104px;object-fit:cover" alt="">
 		            		<div class="carousel-card-content">
 		            			<h5>Asuransi Kendaraan Bermotor</h5>
 		            			<p class="line-clamp line-clamp-3">Asuransi Kerugian Partial & Asuransi Kerugian Total.</p>
@@ -81,7 +81,7 @@
 
 	            	<li>
 	            		<div class="carousel-card">
-		            		<img src="themes/default/assets/images/Asuransi Rekayasa - Engineering Insurance-03.png" style="width:104px;height:104px;object-fit:cover" alt="http://mpm-insurance.com/0_repository/images/59_1small_cash.png">
+		            		<img src="themes/default/assets/images/Asuransi Rekayasa - Engineering Insurance-03.png" style="width:104px;height:104px;object-fit:cover" alt="">
 		            		<div class="carousel-card-content">
 		            			<h5>Asuransi Rekayasa</h5>
 		            			<p class="line-clamp line-clamp-3">Asuransi Konstruksi, Asuransi Pemasangan Mesin, Asuransi Alat Berat, Asuransi Peralatan Elektronik, Asuransi Mesin & Lainnya.</p>
@@ -94,7 +94,7 @@
 
 	            	<li>
 	            		<div class="carousel-card">
-		            		<img src="themes/default/assets/images/Asuransi Kredit & Penjaminan - Credit & Surety Bond  Guarantee-04.png" style="width:104px;height:104px;object-fit:cover" alt="http://mpm-insurance.com/0_repository/images/63_1small-alatberat.png">
+		            		<img src="themes/default/assets/images/Asuransi Kredit & Penjaminan - Credit & Surety Bond  Guarantee-04.png" style="width:104px;height:104px;object-fit:cover" alt="">
 		            		<div class="carousel-card-content">
 		            			<h5>Asuransi Kredit & Penjaminan</h5>
 		            			<p class="line-clamp line-clamp-3">Jaminan Penawaran, Jaminan Uang Muka, Jaminan Pelaksanaan, Jaminan Pemeliharaan, Jaminan Kontra Bank Garansi, Asuransi Kredit & Lainnya.</p>
@@ -106,7 +106,7 @@
 
 	            	<li>
 	            		<div class="carousel-card">
-		            		<img src="themes/default/assets/images/Asuransi Pengangkutan Barang & Rangka Kapal - Cargo & Marine Hull Insurance-05.png" style="width:104px;height:104px;object-fit:cover" alt="http://mpm-insurance.com/0_repository/images/59_1small_cash.png">
+		            		<img src="themes/default/assets/images/Asuransi Pengangkutan Barang & Rangka Kapal - Cargo & Marine Hull Insurance-05.png" style="width:104px;height:104px;object-fit:cover" alt="">
 		            		<div class="carousel-card-content">
 		            			<h5>Asuransi Kapal & Kargo</h5>
 		            			<p class="line-clamp line-clamp-3">Asuransi Pengangkutan Barang Laut, Asuransi Pengangkutan Barang Udara, Asuransi Pengangkutan Barang Darat, Asuransi Rangka Kapal, Asuransi Protection and Indemnity (P&I) & Lainnya.</p>
@@ -118,7 +118,7 @@
 
 	            	<li>
 	            		<div class="carousel-card">
-		            		<img src="themes/default/assets/images/Asuransi Aneka - Miscellous Insurance-06.png" style="width:104px;height:104px;object-fit:cover" alt="http://mpm-insurance.com/0_repository/images/59_1small_cash.png">
+		            		<img src="themes/default/assets/images/Asuransi Aneka - Miscellous Insurance-06.png" style="width:104px;height:104px;object-fit:cover" alt="">
 		            		<div class="carousel-card-content">
 		            			<h5>Asuransi Aneka</h5>
 		            			<p class="line-clamp line-clamp-3">Asuransi Pencurian, Asuransi Uang, Asuransi Kecelakaan, Asuransi Kesehatan, Asuransi Perjalanan & Lainnya.</p>
@@ -130,7 +130,7 @@
 
 	            	<li>
 	            		<div class="carousel-card">
-		            		<img src="themes/default/assets/images/Asuransi Tanggung Gugat - Liability Insurance-07.png" style="width:104px;height:104px;object-fit:cover" alt="http://mpm-insurance.com/0_repository/images/59_1small_cash.png">
+		            		<img src="themes/default/assets/images/Asuransi Tanggung Gugat - Liability Insurance-07.png" style="width:104px;height:104px;object-fit:cover" alt="">
 		            		<div class="carousel-card-content">
 		            			<h5>Asuransi Tanggung Gugat</h5>
 		            			<p class="line-clamp line-clamp-3">Asuransi Public Liability, Asuransi Product Liability, Asuransi Comprehensive General Liability (CGL), Asuransi Automobile Liability, Asuransi Workmens Compensation, Asuransi Employers Liability & Lainnya.</p>
